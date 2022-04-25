c5c8fd978c154a54c3e4ebb70aaaaeb0e0b6d2e0
Gulnara Tuliakova <gulnaratulyakova@gmail.com>
2022-04-25 17:15:50 +0500

    Add task04

---
+++ task04/Program.cs
@@ -0,0 +1,26 @@
+﻿//Вывести на экран дипапзон чисел от -n до n
+
+
+Console.Write("Введите число: ");
+int num2 =  int.Parse(Console.ReadLine());
+int num1 = -num2;
+if (num2 > 0)
+{
+Console.WriteLine("Диапазон чисел от " + num1 + " до " + num2);
+while (num1 <= num2)
+{
+Console.Write(num1);
+Console.Write(" ");
+ num1++;
+}
+}
+else if (num2 < 0)
+{
+Console.WriteLine("Диапазон чисел от " + num2 + " до " + num1);
+while (num2 <= num1)
+{
+Console.Write(num2);
+Console.Write(" ");
+ num2++;
+}
+}
